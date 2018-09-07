deaaa60ee52a459f6aea85e36c33b2d634df0d04
Maik Fröbe <maik44froebe@web.de>
2018-09-07 23:10:11 +0200

    Initial commit

---
+++ src/main/java/com/empty_stack/autogenerate_parameterized_jenkins_job/ParameterGenerator.java
@@ -0,0 +1,14 @@
+package com.empty_stack.autogenerate_parameterized_jenkins_job;
+
+/**
+ * 
+ * @author Maik Fröbe
+ *
+ */
+public class ParameterGenerator
+{
+	public static void parameters(Object o)
+	{
+		System.out.println(o.getClass());
+	}
+}
