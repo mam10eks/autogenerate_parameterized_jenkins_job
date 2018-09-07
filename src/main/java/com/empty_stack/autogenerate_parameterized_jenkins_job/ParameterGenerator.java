f49819af1727a981a416047c70255cd35e3c614b
Maik Fröbe <maik44froebe@web.de>
2018-09-08 01:12:43 +0200

    bla

--- src/main/java/com/empty_stack/autogenerate_parameterized_jenkins_job/ParameterGenerator.java
+++ src/main/java/com/empty_stack/autogenerate_parameterized_jenkins_job/ParameterGenerator.java
@@ -1,5 +1,9 @@
 package com.empty_stack.autogenerate_parameterized_jenkins_job;
 
+import groovy.lang.Closure;
+import javaposse.jobdsl.dsl.Job;
+import javaposse.jobdsl.dsl.jobs.FreeStyleJob;
+
 /**
  * 
  * @author Maik Fröbe
@@ -7,9 +11,20 @@ package com.empty_stack.autogenerate_parameterized_jenkins_job;
  */
 public class ParameterGenerator
 {
-	public static void parameters(Object o)
+	@SuppressWarnings("serial")
+	public static Closure<Void> parameters(Object o)
 	{
 		System.out.println(nullSaveClassOfObject(o));
+		return new Closure<Void>(null)
+		{
+			@Override
+			public Void call()
+			{
+				System.out.println(nullSaveClassOfObject(getDelegate()));
+				
+				return null;
+			}
+		};
 	}
 	
 	public static String nullSaveClassOfObject(Object o)
